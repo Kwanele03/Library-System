9a9717e5ad7742f0f92a69430a381fd2567094d5
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-01-29 20:49:22 +0200

    Update FirstClass.java

--- FirstClass.java
+++ FirstClass.java
@@ -2,8 +2,7 @@ package package1;
 
 public class FirstClass {
 
-	 public static void main(String[] args) {
-		 		 
+	 public static void main(String[] args) { 		 
 		 
 	// Creating User class object named user. 
 	 User user = new User("Kwanele Ntshele", "2009-06-07"); 
@@ -17,20 +16,15 @@ public class FirstClass {
 	 // Creating an Ebook for Head First Java book
 	 Ebook eBook = new Ebook("Head First Java e-Book", "Bert Bates and Kathy Sierra", 2005, 812, "PDF");
 	 
-	 
-	 
 	// Calling the borrow method from the user object
 	 user.borrow(book); 
-	 
 	
-	 
 	// Printing out user name, birthDay, and age in years.
 	 System.out.printf("My name is %s, I was born on %s. I am now %d years old. ", user.getName(), user.getBirthDay(),user.age());
 	 
 	// Printing out the user who has borrowed the book, and the book whom they have borrowed.
 	 System.out.printf("I, have borrowed this book: %s \n",user.borrowedBooks()); 
 	 
-	
 	 //Printing out AudioBook
 	 System.out.printf("Book Type: " + audioBook + "it plays for %d minutes, (5hrs). \n", audioBook.getLength());
 	 
